fad17f8f7f0e4b10c093fb0357409e87c9998abe
spence <exussum@spences-laptop.spencer.home>
2023-11-17 15:46:38 -0500

    README

---
+++ README.txt
@@ -0,0 +1,3 @@
+The goal of Skimmer is to take in communication from multiple sources, email, github, "other," and learn what's important, then show on screen the most pressing notifcations.
+
+Skimmer is my pet project, soup to nuts, using everything.  It's partially an idealistic demonstration of ability, partially a learning project, all to get the task done.
--- dev-setup.sh
+++ dev-setup.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
 brew install ariga/tap/atlas
+
+cat << EOF > flask/.env
+GOOGLE_CLIENT_ID=unset
+GOOGLE_CLIENT_SECRET=unset
+GOOGLE_REDIRECT_URL="http://localhost:8000/auth/code"
+MEMCACHED_KEY_PREFIX=skimmer
+MEMCACHED_SERVER=memcached:11211
+REACT_HOME_URL=http://localhost:8080/
+FLASK_PERMANENT_SESSION_LIFETIME=5
+FLASK_SQLALCHEMY_DATABASE_URI=postgresql://skimmer:skimmer@db/skimmer
+EOF
