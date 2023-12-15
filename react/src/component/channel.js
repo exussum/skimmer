208a0e524345cb9f72d5d5650ebb6cdc5588d93c
exussum <exussum@gmail.com>
2023-12-15 15:28:50 -0500

    Wire in groups, clean up content rendering

--- react/src/component/channel.js
+++ react/src/component/channel.js
@@ -12,7 +12,7 @@ export const ChannelNav = ({ channels, className, onSelect }) => {
   return (
     <div className="flex">
       <ul className={`flex-column space-y space-y-4 ${className}`}>
-        <li>{t("Channels")}</li>
+        <li>{t("Channels Title")}</li>
         {buttons}
       </ul>
     </div>
@@ -35,22 +35,28 @@ const Channel = ({ id, channel_type, onClick }) => {
   );
 };
 
-export const MessageList = ({ data }) => {
-  const contents = data.map((e) => <Item key={`${e.id}`} from={e.from} subject={e.subject} sent={e.sent} />);
+export const MessageList = ({ data, groups }) => {
+  const contents = data.map((e) => (
+    <Item groups={groups} key={`${e.id}`} from={e.from} subject={e.subject} sent={e.sent} />
+  ));
   return <div className="bg-menu flex-1 flex flex-col overflow-hidden">{contents}</div>;
 };
 
-const Item = ({ id, from, subject, sent }) => {
+const Item = ({ id, from, subject, sent, groups }) => {
   const localDate = new Date(sent).toLocaleString("en-US");
+
+  const selectItems = groups.map((e) => (
+    <option value={e.id} key={e.id}>
+      {e.name}
+    </option>
+  ));
 
   return (
     <div className="flex">
-      <div className="flex-0 p-2">
-        <input type="checkbox" />
-      </div>
       <div className="basis-64 p-2 text-ellipsis overflow-hidden whitespace-nowrap">{from}</div>
       <div className="flex-1 p-2 text-ellipsis overflow-hidden whitespace-nowrap">{subject}</div>
       <div className="basis-64 p-2">{localDate}</div>
+      <select className="bg-menu p-2">{selectItems}</select>
     </div>
   );
 };
--- react/src/views/content.js
+++ react/src/views/content.js
@@ -2,12 +2,13 @@ import { useState, useEffect, useContext, useCallback, useRef } from "react";
 import { AuthContext } from "../api/auth";
 import { addGroup, deleteGroup, fetchGroups } from "../api/group";
 import { getChannel } from "../api/channel";
-import { useQueryClient, useQuery, useMutation } from "react-query";
+import { useQueryClient, useQueries, useMutation } from "react-query";
 import { MessageList } from "../component/channel";
 import { GroupManager } from "../component/group";
 import { useTranslation } from "react-i18next";
 import { Button } from "flowbite-react";
 
+// vv move this into a component
 export const NonButtonDropDown = (props) => {
   const dropDownRef = useRef(null);
 
@@ -33,12 +34,13 @@ export const NonButtonDropDown = (props) => {
     </div>
   );
 };
+// ^^ move this into a component
 
 export const Content = () => {
   const { ctx } = useContext(AuthContext);
   const { t } = useTranslation();
-
   const [visible, setVisible] = useState(false);
+
   const click = (e) => {
     setVisible(!visible);
     e.stopPropagation();
@@ -50,10 +52,11 @@ export const Content = () => {
         <Button onClick={click} className="bg-popup w-min whitespace-nowrap">
           {t("Show group manager")}
         </Button>
-        <NonButtonDropDown visible={visible} setVisible={setVisible}>
-          <LoadGroupManager channelId={ctx.selectedChannelId} className="border-2" />
-        </NonButtonDropDown>
-        <LoadContent channelId={ctx.selectedChannelId} />
+        <LoadContent
+          channelId={ctx.selectedChannelId}
+          groupManagerVisible={visible}
+          setGroupManagerVisible={setVisible}
+        />
       </div>
     );
   } else {
@@ -61,58 +64,54 @@ export const Content = () => {
   }
 };
 
-export const LoadGroupManager = ({ channelId, className }) => {
-  const [, setProcessing] = useState(false);
-  const key = ["groups", channelId];
-  const { isLoading, data } = useQuery(key, fetchGroups);
+const LoadContent = ({ channelId, groupManagerVisible, setGroupManagerVisible }) => {
   const queryClient = useQueryClient();
+  const [processing, setProcessing] = useState(false);
+
+  const queryResults = useQueries({
+    queries: [
+      { queryKey: ["channel", channelId], queryFn: getChannel, refetchInterval: 6000 },
+      { queryKey: ["groups", channelId], queryFn: fetchGroups },
+    ],
+  });
+
+  const channelResults = queryResults[0];
+  const groupResults = queryResults[1];
 
   const addMutation = useMutation(addGroup, {
     onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: channelId });
       setProcessing(false);
-      queryClient.invalidateQueries({ queryKey: key });
     },
   });
+
   const deleteMutation = useMutation(deleteGroup, {
     onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: channelId });
       setProcessing(false);
-      queryClient.invalidateQueries({ queryKey: key });
     },
   });
 
-  const stuff = (bigId, id) => {};
-
   return (
-    <GroupManager
-      processing={isLoading}
-      data={data}
-      className={className}
-      selectGroup={(id) => stuff("select", id)}
-      deleteGroup={(id) => {
-        setProcessing(true);
-        deleteMutation.mutate({ channelId: channelId, id: id });
-      }}
-      addGroup={(name) => {
-        if (name) {
-          setProcessing(true);
-          addMutation.mutate({ channelId: channelId, name: name });
-        }
-      }}
-    />
+    <>
+      <NonButtonDropDown visible={groupManagerVisible} setVisible={setGroupManagerVisible}>
+        <GroupManager
+          processing={processing}
+          data={groupResults.data || []}
+          className="border-2"
+          deleteGroup={(id) => {
+            setProcessing(true);
+            deleteMutation.mutate({ channelId: channelId, id: id });
+          }}
+          addGroup={(name) => {
+            if (name) {
+              setProcessing(true);
+              addMutation.mutate({ channelId: channelId, name: name });
+            }
+          }}
+        />
+      </NonButtonDropDown>
+      <MessageList data={channelResults.data || []} groups={groupResults.data || []} />
+    </>
   );
-};
-
-const LoadContent = ({ channelId }) => {
-  const [items, setItems] = useState([]);
-  const { isLoading, data } = useQuery(["channel", channelId], getChannel, { refetchInterval: 6000 });
-
-  useEffect(() => {
-    if (!isLoading && data) {
-      setItems(data);
-    } else {
-      setItems([]);
-    }
-  }, [data, setItems, isLoading]);
-
-  return <MessageList data={items} />;
 };
